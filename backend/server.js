8da915f3710177e88e7ff52c4a9a6c2bb49525b5
Sávio Iesous Rocha Rodrigues <rodriguessavio68@gmail.com>
2025-09-11 00:24:56 -0300

    bbbb

--- backend/server.js
+++ backend/server.js
@@ -2,37 +2,30 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import duvidasRoutes from "./routes/duvidas.js";
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 import path from "path";
 import { fileURLToPath } from "url";
 
+const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-
+// Middleware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://rodriguessavio68_db_user:savio497@cluster0.zwvuyto.mongodb.net/duvidasDB?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+
 app.use("/duvidas", duvidasRoutes);
 
 
-// Middleware
-app.use(express.json());
-
-// Servir frontend
-app.use(express.static("public"));
-
 // Rotas do backend
+app.use("/duvidas", duvidasRoutes);
 app.get("/api", (req, res) => {
   res.json({ message: "Backend funcionando!" });
 });
